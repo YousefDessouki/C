293777e976758704d24ee46faef27913d6582132
YousefDessouki <124085074+YousefDessouki@users.noreply.github.com>
2024-01-22 11:00:08 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int x;
+    scanf("%d",&x);
+
+    int checker=0;
+    int flag=1;
+    int nod=0;
+
+    while(flag==1)
+    { 
+        nod++;
+        checker=x/pow(10,nod);
+        
+        
+        
+        if(checker==0)
+        {flag=0;}
+        
+       
+    }
+
+int reverseanswer = 0;
+int playnum=0;
+
+for (int i = 1; i <= nod; i++) 
+{
+    playnum = (x % (int)pow(10, i));
+    playnum=playnum/(int)pow(10,i-1);
+    printf("%d\n",playnum);
+    reverseanswer=reverseanswer+(playnum*(int)pow(10,nod-i));
+}
+printf("%d\n",reverseanswer);
+
+
+
+
+
+
+
+
+
+
+
+
+}
